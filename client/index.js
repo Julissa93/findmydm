e037a22043f7bf1b902c558e46d2a2fc61b0e237
julissa93 <jnapoletano28@gmail.com>
2021-03-25 20:26:05 -0400

    first kermit

---
+++ client/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+    return (
+        <h1>Welcome to our D&D app!</h1>
+    )
+}
+ReactDOM.render(<App />, document.getElementById("app"))
---
+++ server/index.js
@@ -0,0 +1,42 @@
+const path = require('path')
+const express = require('express')
+const morgan = require('morgan')
+const PORT = process.env.PORT || 8080
+const app = express()
+module.exports = app
+
+
+// static file-serving middleware
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
+// any remaining requests with an extension (.js, .css, etc.) send 404
+app.use((req, res, next) => {
+    if (path.extname(req.path).length) {
+      const err = new Error('Not found')
+      err.status = 404
+      next(err)
+    } else {
+      next()
+    }
+})
+
+// sends index.html
+app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public/index.html'))
+})
+
+// error handling endware
+app.use((err, req, res, next) => {
+    console.error(err)
+    console.error(err.stack)
+    res.status(err.status || 500).send(err.message || 'Internal server error.')
+})
+
+const startListening = () => {
+    // start listening (and create a 'server' object representing our server)
+    const server = app.listen(PORT, () =>
+        console.log(`Mixing it up on port ${PORT}`)
+    )
+}
+
+startListening()
